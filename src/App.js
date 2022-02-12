d2c7e85d65144817eef2e176f4c02e2f331b4b4a
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-02-12 11:58:37 +0100

    Add routings

--- src/App.js
+++ src/App.js
@@ -1,84 +1,23 @@
-import React, { useEffect, useState } from "react";
-import "./styles/App.css";
-import PostList from "./components/PostList";
-import PostForm from "./components/PostForm";
-import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal";
-import MyButton from "./components/UI/button/MyButton";
-import { usePosts } from "./hooks/usePosts";
-import PostService from "./API/PostService";
-import Loader from "./components/UI/Loader/Loader";
-import useFetching from "./hooks/useFetching";
-import { getPagesCount } from "./utils/pages";
-import MyPagination from "./components/UI/pagination/MyPagination";
+import React from "react";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import About from "./components/pages/About";
+import Posts from "./components/pages/Posts";
+import './styles/App.css';
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: "0k", title: "Bitle", body: "Aome text" },
-    { id: "1k", title: "Gitle", body: "bome text" },
-    { id: "2k", title: "Aitle3", body: "Come text" },
-    { id: "3k", title: "Citle4", body: "Lome textdfbdfb" },
-  ]);
-  const [filter, setFilter] = useState({ sort: "title", query: "" });
-  const [modal, setModal] = useState(false);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [totalPages, setTotalPages] = useState(0);
-  const [page, setPage] = useState(1);
-  const limit = 10;
-
-  const [fetchPosts, isPostsLoading, postsError] = useFetching(async () => {
-    const response = await PostService.getAll(limit, page);
-    setPosts(response.data);
-    const totalCount = response.headers["x-total-count"];
-    setTotalPages(getPagesCount(totalCount, limit));
-  });
-
-  const createPost = (post) => {
-    setPosts([...posts, post]);
-    setModal(false);
-  };
-
-  const removePost = (id) => {
-    setPosts(
-      posts.filter((e) => {
-        if (e.id !== id) return e;
-      })
-    );
-  };
-
-  useEffect(() => {
-    fetchPosts();
-  }, [page]);
-
   return (
-    <div className="App">
-      <MyButton style={{ marginTop: "30px" }} onClick={() => setModal(true)}>
-        Создать пост
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} posts={posts} />
-      </MyModal>
-
-      <hr style={{ margin: "15px 0" }} />
-      <div>
-        <PostFilter filter={filter} setFilter={setFilter} />
-      </div>
-      {postsError && <div>Error {postsError}</div>}
-
-      {isPostsLoading ? (
-        <div style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
-          <Loader />
-        </div>
-      ) : (
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} />
-      )}
-
-      <MyPagination 
-        totalPages={totalPages} 
-        changePage={setPage} 
-        page={page} 
-      />
-    </div>
+    <BrowserRouter>
+      <nav className="navbar">
+        <ul className="navbar_links">
+          <li><NavLink to="/posts">Posts</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
